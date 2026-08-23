a4ada9280deada2e9d55c8692579b4a77fbd13ca
Tabish Sajwani <tabishsajwani@hotmail.com>
2026-08-23 15:14:32 +0500

    installed react + typescript + tailwind + shadcn + lucide react for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+import { Camera } from "lucide-react"
+
+function App() {
+
+  return (
+    <>
+      <Button><Camera /></Button>
+    </>
+  )
+}
+
+export default App
